dfaa0b4d4b94884d7c414e7de5ba72215bcfa1cd
mughal2791 <35995692+mughal2791@users.noreply.github.com>
2018-02-02 10:24:28 +0500

    Add files via upload

---
+++ numr.R
@@ -0,0 +1,16 @@
+# Number, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+numr <- function(x) {
+  return(x+1)
+}
